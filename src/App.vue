56e7b6f985b95978c0b9495465ee97f7fda9db1c
kh11884 <kh11884@gmail.com>
2020-05-11 21:16:43 +0700

    Добавлен шаблон карточки со списком (опыт работы) на фронте.

--- src/App.vue
+++ src/App.vue
@@ -32,37 +32,46 @@
           </div>
         </v-list-item>
 
+        <v-list-group
+          prepend-icon="mdi-school"
+          no-action
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Образование</v-list-item-title>
+          </template>
+
+            <v-list-item link>
+              <div>
+                <router-link tag="div" to="/study">
+                  <v-row>
+                    <v-icon>mdi-school</v-icon>
+                    <v-list-item-content class="ml-2">
+                      <v-list-item-title>Высшее образование</v-list-item-title>
+                    </v-list-item-content>
+                  </v-row>
+                </router-link>
+              </div>
+            </v-list-item>
+
+            <v-list-item link>
+              <div>
+                <router-link tag="div" to="/additionalEducation">
+                  <v-list-item-action>
+                    <v-row>
+                      <v-icon>mdi-cast-education</v-icon>
+                      <v-list-item-content class="ml-2">
+                        <v-list-item-title>Доп.обучение</v-list-item-title>
+                      </v-list-item-content>
+                    </v-row>
+                  </v-list-item-action>
+                </router-link>
+              </div>
+            </v-list-item>
+        </v-list-group>
+
         <v-list-item link>
           <div>
-            <router-link tag="div" to="/study">
-              <v-row>
-                <v-icon>mdi-school</v-icon>
-                <v-list-item-content class="ml-2">
-                  <v-list-item-title>Образование</v-list-item-title>
-                </v-list-item-content>
-              </v-row>
-            </router-link>
-          </div>
-        </v-list-item>
-
-        <v-list-item>
-          <div>
-            <router-link tag="div" to="/additionalEducation">
-              <v-list-item-action>
-                <v-row>
-                  <v-icon>mdi-cast-education</v-icon>
-                  <v-list-item-content class="ml-2">
-                    <v-list-item-title>Курсы (доп. обучение)</v-list-item-title>
-                  </v-list-item-content>
-                </v-row>
-              </v-list-item-action>
-            </router-link>
-          </div>
-        </v-list-item>
-
-        <v-list-item link>
-          <div>
-            <router-link tag="div" to="/Skills">
+            <router-link tag="div" to="/skills">
               <v-list-item-action>
                 <v-row>
                   <v-icon>mdi-tools</v-icon>
@@ -75,7 +84,7 @@
           </div>
         </v-list-item>
 
-        <v-list-item>
+        <v-list-item link>
           <div>
             <router-link tag="div" to="/recommendations">
               <v-list-item-action>
@@ -127,5 +136,5 @@
     data: () => ({
       drawer: null,
     }),
-  };
+  }
 </script>
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,12 +1,79 @@
 <template>
-    <v-card>
-      <v-card-title>Опыт работы</v-card-title>
-    </v-card>
+  <v-card
+    class="mx-auto"
+    max-width="640"
+    outlined>
+    <v-card-title>Опыт работы</v-card-title>
+    <v-card-text>
+      <v-list v-for="row in rows">
+        <v-list-item>
+          <v-row>
+            <v-col cols="3">{{row.date}}</v-col>
+            <v-col>
+              <p class="font-weight-black">{{row.place}}</p>
+              <p class="font-weight-bold">{{row.position}}</p>
+              <p>{{row.responsibility}}</p>
+            </v-col>
+          </v-row>
+        </v-list-item>
+      </v-list>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
   export default {
-    name: 'Experience'
+    name: 'Experience',
+    data () {
+      return {
+        rows: [
+          {
+            date: 'Июль 2019 — по настоящее время',
+            place: 'ООО "С7 КАРГО"',
+            position: 'Старший декларант',
+            responsibility: 'Старший смены. организация работы в смене: распределение текущей работы, контроль исполнения.'
+          },
+          {
+            date: 'Март 2008 — июль 2019',
+            place: 'ООО "С7 КАРГО"',
+            position: 'Специалист по таможенному оформлению',
+            responsibility: 'таможенное оформление товаров перемещаемых в рамках внешнеторговых контрактов.'
+          },
+          {
+            date: 'Апрель 2004 — март 2008',
+            place: 'ОАО "Авиакомпания "Сибирь"',
+            position: 'Эксперт по грузовым перевозкам',
+            responsibility: 'Создание и поддержание агентской сети продаж грузовых авиаперевозок.'
+          },
+          {
+            date: 'Июнь 2002 — март 2004',
+            place: 'ОАО "Сибакадембанк"',
+            position: 'Специалист управления маркетинга и рекламы',
+            responsibility: 'Анализ рынка розничных банковских услуг. Разработка, внедрение и продвижение банковских продуктов и услуг. Формирование имиджа банка. Работа с подрядчиками по производству рекламы.'
+          }
+        ],
+        testField: 0,
+      }
+    },
+    methods: {
+      getWeekCapacity: function () {
+        this.showSpinner = true
+        var self = this
+        self.showWeekCapacity = false
+        $.ajax({
+          type: 'POST',
+          url: '/capacityCalculator/getWeekCapacity',
+          contentType: 'application/json'
+        }).done(function (responce) {
+          self.rows = responce
+          self.showWeekCapacity = true
+        }).fail(function () {
+          self.errorMessage = 'The request could not be completed, please try again later.'
+        }).always(function () {
+          self.showSpinner = false
+        })
+      }
+    },
   }
 </script>
 
